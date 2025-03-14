2b9358977c768578ca49a35b53641a98292b3e5e
avidars2 <71943836+avidars2@users.noreply.github.com>
2025-03-14 00:53:27 -0400

    med 1 completed

--- js_exercises/js101-js119-small-problems/medium-1/09-fibonacci-memoization.js
+++ js_exercises/js101-js119-small-problems/medium-1/09-fibonacci-memoization.js
@@ -1,26 +1,72 @@
 
 
 let fibArray = [1, 1];
+let fibObj = {
+  1: 1n,
+  2: 1n
+}
 //If fibArray length === nth, access fibArray[nth - 1] value
+//Perform fibonacci recursion
+//Check if answer is stored in database before performing function
+//If it is, return that value to avoid repeating calculation
+//If it isn't, add that value to the database
+/**
+ * 
+ */
 
+let iterations = 0;
 function fibonacci(nth) {
-  if (fibArray.length )
-  if (nth === 2 || nth === 1) {
-    return 1;
+  iterations++;
+  //Check if fibObj(nth) exists
+  //If so, return that value
+  if (fibObj.hasOwnProperty(nth)) return fibObj[nth];
+  if (nth === 2n || nth === 1n) {
+    return 1n;
   }
 
-  return fibonacci(nth - 1) + fibonacci(nth - 2);
+  //If code makes it this far, add result to data base before returning it
+  fibObj[nth] = fibonacci(nth - 1n) + fibonacci(nth - 2n);
+  return fibObj[nth];
  }
 
- function fibonacci(nth) {
-  let fibArray = [1, 1];
+//  {
+//  function fibonacci(nth) { //Original function to compare speed
+//   if (nth === 2n || nth === 1n) {
+//     return 1n;
+//   }
 
-  while (fibArray.length <= nth) {
-    let fibArrayLen = fibArray.length;
-    fibArray.push(fibArray[fibArrayLen - 1] + fibArray[fibArrayLen - 2]);
+//   return fibonacci(nth - 1n) + fibonacci(nth - 2n);
+//  }
+//  }
+
+
+ function fibonacciExpl(nth) {
+  //Check if fibObj(nth) exists
+  //If so, return that value
+  if (fibObj.hasOwnProperty(nth)) {
+    console.log(`We already saved F${nth}, returning ${fibObj[nth]}`);
+    return fibObj[nth];
   }
 
-  console.log(fibArray[nth - 1]);
-  return fibArray[nth - 1];
-
+  //If code makes it this far, add result to data base before returning it
+  console.log(`We don't have F${nth}, calculating...`);
+  fibObj[nth] = fibonacciExpl(nth - 1n) + fibonacciExpl(nth - 2n);
+  console.log(`returning... ${fibObj[nth]}`);
+  return fibObj[nth];
  }
+
+//  console.log(fibonacciExpl(100n));
+//  console.log(fibObj);
+//  console.log(fibonacci(40n));
+
+let cache = [0n, 1n, 1n];
+let calls = 0;
+
+function fibonacciImp(nth) {
+  calls++;
+  //Check if cache has nth value
+  cache[nth] = cache[nth] ?? fibonacciImp(nth - 1n) + cache[nth - 2n];
+  return cache[nth];
+}
+ console.log(fibonacci(8366n), `\n Iterations: ${iterations}`); //Max calls arg 8366n, 16729 iterations
+ console.log(fibonacciImp(8965n), `\n Calls: ${calls}`); //Max calls arg 8965n, 8964 iterations
---
+++ js_exercises/js101-js119-small-problems/test2.js
@@ -0,0 +1,4 @@
+let [arr1, arr2] = [[]];
+
+console.log(arr1)
+console.log(arr2)
