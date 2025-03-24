f29d010e2c3650c866871c5afddbeb2443d4be4e
Avinash Darshan <avidars2@gmail.com>
2025-03-24 17:37:31 -0400

    Create session.js

---
+++ js_exercises/js101-js119-small-problems/ta-sessions/session.js
@@ -0,0 +1,10 @@
+let exp = num1 => num2 => num1 ** num2;
+console.log(exp(3));
+
+// function exp(num1) {
+//   return function x(num2) {
+//     return num1 ** num2
+//   }
+// }
+
+// undefined return value
